552e4e0104ed00c3b468d6a2e7936d1530fbeb91
JungeunKwon <wow_v_v@naver.com>
2019-11-29 16:12:22 +0900

    BaekJoon #15685-20191115

---
+++ BaekJoon/Sol_15685_BJ.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sol_15685_BJ {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+
+	}
+
+}
